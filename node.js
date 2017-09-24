b43746b7302883be7fe4c60e26acb9afc2d678ce
Raphael Long <rafilong42@gmail.com>
2017-09-23 19:12:51 -0500

    Update mongo cluster

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var uri = "mongodb://42mileslong:1D7h6qhY2r2sn6Dn@test-cluster-shard-00-00-w1irb.mongodb.net:27017,test-cluster-shard-00-01-w1irb.mongodb.net:27017,test-cluster-shard-00-02-w1irb.mongodb.net:27017/test?ssl=true&replicaSet=test-cluster-shard-0&authSource=admin";
+var uri = "mongodb://heroku:ilqHPiWgfHepCpNC@narc-cluster-shard-00-00-uij1v.mongodb.net:27017,narc-cluster-shard-00-01-uij1v.mongodb.net:27017,narc-cluster-shard-00-02-uij1v.mongodb.net:27017/test?ssl=true&replicaSet=narc-cluster-shard-0&authSource=admin";
 
 function query(data) {
 	MongoClient.connect(uri, function(err, db) {
