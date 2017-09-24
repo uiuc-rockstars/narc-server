aabd4132c7ee4575cf77d66bb1d4f6b36b4c9e60
Raphael Long <rafilong42@gmail.com>
2017-09-23 19:04:17 -0500

    Style change

--- node.js
+++ node.js
@@ -23,6 +23,7 @@ var serv = require("http").Server(app);
 app.get("/", function (req, res) {
 	res.sendFile(__dirname+ "/client/index.html");
 });
+
 app.use("/client", express.static(__dirname + "/client"));
 serv.listen(3000);
 
