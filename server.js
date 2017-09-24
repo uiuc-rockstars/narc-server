2cb78387832e3d5fc078c5019def65d792139cd4
Raphael Long <rafilong42@gmail.com>
2017-09-24 03:40:25 -0500

    Websocket solves everything

---
+++ server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Website to Server
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+
+    ws.on('message', function (msg) {
+	console.log('Received message');
+	query(JSON.parse(msg));
+    });
+
+});
+
+
+setInterval(() => {
+    wss.clients.forEach((client) => {
+	client.send(new Date().toTimeString());
+    });
+}, 1000);
+
+
+// Server to Database
+
+var MongoClient = require('mongodb').MongoClient;                               
+                                                                                
+var uri = "mongodb://heroku:ilqHPiWgfHepCpNC@narc-cluster-shard-00-00-uij1v.mon\
+godb.net:27017,narc-cluster-shard-00-01-uij1v.mongodb.net:27017,narc-cluster-sh\
+ard-00-02-uij1v.mongodb.net:27017/test?ssl=true&replicaSet=narc-cluster-shard-0\
+&authSource=admin";                                                             
+                                                                                
+function query(data) {                                                          
+    MongoClient.connect(uri, function(err, db) {                                
+        console.log("Connected to database");                                   
+                                                                                
+        db.collection("reports").insertOne(data,function(err,res){              
+            console.log("Document inserted");                                   
+                                                                                
+            db.close();                                                         
+        });                                                                     
+    });                                                                         
+}
