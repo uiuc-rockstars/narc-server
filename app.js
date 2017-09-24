9ba5580b2d1f4e573be83d6f8edc7de103122155
Raphael Long <rafilong42@gmail.com>
2017-09-24 00:11:45 -0500

    Change polling duration

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var port = process.env.PORT || 3000;
 serv.listen(port);
 
 io.set("transports", ["xhr-polling"]); 
-io.set("polling duration", 10); 
+io.set("polling duration", 100); 
 
 io.sockets.on("connection", function(socket){
     socket.emit("Connected");
