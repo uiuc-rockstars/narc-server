8261cd01331916ed6496a03fef445ebbc88c83a5
Raphael Long <rafilong42@gmail.com>
2017-09-24 09:01:59 -0500

    Add missing parenthesis

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ wss.on('connection', (ws) => {
     ws.on('message', function (msg) {
 	console.log('Received message');
 	var data = JSON.parse(msg);
-	data['timestamp'] = newDate.toTimeStamp();
+	data['timestamp'] = new Date().toTimeStamp();
 	query(data);
     });
 
